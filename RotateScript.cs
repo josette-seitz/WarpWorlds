43eb3f640cf8ec3da7489fde55d7debc775a6361
Josette Seitz <josette.seitz@tma1.com>
2018-05-04 03:42:49 -0400

    Update RotateScript.cs

--- RotateScript.cs
+++ RotateScript.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateScript : MonoBehaviour {
-
 	void Update () {
 		transform.Rotate(new Vector3(5,30,20) * Time.deltaTime);
 	}
