822975fe9d805352c062cab76f2d4b303491201b
Josette Seitz <josette.seitz@tma1.com>
2018-05-04 03:41:53 -0400

    Update RotateScript.cs

--- RotateScript.cs
+++ RotateScript.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 
 public class RotateScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
 		transform.Rotate(new Vector3(5,30,20) * Time.deltaTime);
-
 	}
 }
