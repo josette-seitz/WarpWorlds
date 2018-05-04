7345f38ec1826c44cc659e126033e13a6e1523e8
Josette Seitz <josette.seitz@tma1.com>
2018-05-04 03:36:08 -0400

    Update ScrollScript.cs

--- ScrollScript.cs
+++ ScrollScript.cs
@@ -3,22 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScrollScript : MonoBehaviour {
+		public float scrollSpeed = 50;
 
-	public float scrollSpeed = 50;
+		void Update () {
+		Vector3 pos = transform.position;
+		Vector3 localVectorUp = transform.TransformDirection(0,1,0);
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-	Vector3 pos = transform.position;
-	Vector3 localVectorUp = transform.TransformDirection(0,1,0);
-
-	pos += localVectorUp * scrollSpeed * Time.deltaTime;
-	transform.position = pos;
-		
+		pos += localVectorUp * scrollSpeed * Time.deltaTime;
+		transform.position = pos;
 	}
 }
