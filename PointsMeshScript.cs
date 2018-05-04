949a70ebdc001faa62cd4cff11c0c460f1cefc6a
Josette Seitz <josette.seitz@tma1.com>
2018-05-04 03:43:26 -0400

    Update PointsMeshScript.cs

--- PointsMeshScript.cs
+++ PointsMeshScript.cs
@@ -3,13 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PointsMeshScript : MonoBehaviour {
-
-
-	// Use this for initialization
 	void Start () {
-
 		MeshRenderer pathPoints = this.GetComponent<MeshRenderer>();
 		pathPoints.enabled = false;
-		
 	}
 }
